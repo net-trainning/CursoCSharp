2610256021400b89475a5de1dcf15923dc9670b1
karenRamos <karen.ramos.carpio@gmail.com>
2026-03-19 14:34:21 -0500

    add condicionales

---
+++ 2Condicionales/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+//2) programa que busque en la cadena "esto es una cadena de prueba porque estoy empezando a programar en Csharp" las palbras prueba y programar
+// si lo encuentra que emprima por pantalla "encontrado"
+
+string texto = "esto es una cadena de prueba porque estoy empezando a programar en Csharp";
+
+if (texto.Contains("prueba") && texto.Contains("programar"))
+{
+    Console.WriteLine("encontrado");
+}
